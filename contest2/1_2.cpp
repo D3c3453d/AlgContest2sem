1cd67218f31ab07cc5991f86424a8eac45523ebb
D3c3453d <48224226+D3c3453d@users.noreply.github.com>
2022-04-09 17:44:33 +0300

    contest2
    https://contest.yandex.ru/contest/17492/enter/

---
+++ contest2/1_2.cpp
@@ -0,0 +1,63 @@
+﻿/*
+Задано N точек на плоскости. Указать (N−1)-звенную несамопересекающуюся незамкнутую ломаную, проходящую через все эти точки.
+Указание: стройте ломаную в порядке возрастания x-координаты. 
+Если имеются две точки с одинаковой x-координатой, то расположите раньше ту точку, у которой y-координата меньше.
+
+Пример:
+Ввод:
+4
+0 0
+1 1
+1 0
+0 1
+
+Вывод:
+0 0
+0 1
+1 0
+1 1
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct A{
+    int x, y;
+    int ind;
+    bool operator<( A r) const{
+        if (x > r.x)return false;
+        if (x < r.x)return true;
+        return y < r.y;
+    }
+};
+
+int max(int a, int b){
+    if (a > b)return a;
+    return b;
+}
+
+
+
+std::istream& operator>> (std::istream &in, A &p)
+{
+    in >> p.x >> p.y;
+    return in;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<A> arr(n);
+    for (int i = 0; i < n; ++i){
+        cin >> arr[i];
+        arr[i].ind = i;
+    }
+    sort(arr.begin(), arr.end());
+    for (auto i: arr){
+        cout << i.x << " " << i.y <<endl;
+    }
+}
---
+++ contest2/1_5.cpp
@@ -0,0 +1,100 @@
+﻿/*
+В базе данных хранится N записей, вида (Name, a1, a2, … , ak) — во всех записях одинаковое число параметров. 
+На вход задачи подаётся приоритет полей — перестановка на числах 1,...,k — записи нужно вывести в соответствии с этим приоритетом. 
+В случае, если приоритет полей таков: 3 4 2 1, то это следует воспринимать так: надо читать как: приоритет значений из 3 колонки самый высокий, 
+приоритет значений из колонки 4 ниже, приоритет значений из колонки 2 ещё ниже, а приоритет значений из колонки 1 самый низкий.
+
+Формат ввода:
+N (1⩽N⩽1000) k (1⩽k;⩽10) p1 p2 ... pk (перестановка на k числах, разделитель пробел) N строк вида Name a1 a2 ... ak (разделитель — пробел)
+
+Формат вывода:
+N строк с именами согласно приоритету
+
+Пример:
+Ввод:
+3
+3
+2 1 3
+A 1 2 3
+B 2 1 3
+C 3 1 2
+
+Вывод:
+A
+C
+B
+
+*/
+
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+vector<int> priority;
+
+struct A{
+    string name;
+    vector<int> data;
+    A(){}
+    A(string name, int k):name(std::move(name)){
+        data.resize(k);
+    }
+    A( int k){
+        data.resize(k);
+    }
+    bool operator>( A& r) const{
+
+        for (auto i: priority){
+            if (data[i-1] > r.data[i-1])return true;
+            if (data[i-1] < r.data[i-1])return false;
+        }
+        return false;
+    }
+
+};
+
+
+
+int max(int a, int b){
+    if (a > b)return a;
+    return b;
+}
+
+std::ostream& operator<< (std::ostream &out, const A &point)
+{
+    out << point.name ;
+    return out;
+}
+
+std::istream& operator>> (std::istream &in, A &p)
+{
+    in >> p.name;
+    for (int & i : p.data){
+        cin >> i;
+    }
+    return in;
+}
+
+
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    priority.resize(k);
+    for (auto i = priority.begin(); i != priority.end(); ++ i){
+        cin >> *i;
+    }
+    vector<A> arr(n, A {k});
+    for (auto i = arr.begin(); i != arr.end(); ++ i){
+        cin >> *i;
+    }
+    sort(arr.begin(), arr.end(), []( A& a,  A& b){ return a > b;});
+    for (auto i: arr){
+        cout <<  i << endl;
+    }
+}
